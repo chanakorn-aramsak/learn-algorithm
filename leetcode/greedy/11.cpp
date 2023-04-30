c5bc131278de7d191e20963d3874612c4ca66d93
chanakorn aramsak <solo20113@gmail.com>
2023-04-30 13:16:41 +0700

    explain how it use greedy

--- leetcode/greedy/11.cpp
+++ leetcode/greedy/11.cpp
@@ -1,13 +1,17 @@
-class Solution {
+class Solution
+{
 public:
-    int maxArea(vector<int>& height) {
-       int ans = 0, n = height.size();
-       int i = 0; int j = n-1;
-
-       while (i<j){
-            ans = max(ans, (j-i) * min(height[i], height[j]));
-            if (height[i] < height[j]) i++;
+    int maxArea(vector<int> &height)
+    {
+        // เลือกสิ่งที่ดีสุดคือเลือก area มากสุด
+        int ans = 0;
+        int i = 0, j = height.size() - 1;
+        while (i < j)
+        {
+            ans = max(ans, (j-i)*min(height[i],height[j]));
+            if (height[i]<height[j]) i++;
             else j--;
-       }
+        }
+        return ans;
     }
 };
--- leetcode/greedy/44.cpp
+++ leetcode/greedy/44.cpp
@@ -3,6 +3,8 @@ class Solution
 public:
     bool isMatch(string s, string p)
     {
+        // เลือกสิ่งที่ดีสุดคือพยายามคิดว่าถ้าเจอตัวที่ไม่ match ให้คิดว่าเป็น * ไว้ก่อน
+        // ถ้า match ให้คิดว่าไม่ใช่ *
         int i = 0, j = 0, star = -1, match = 0;
         while (i < s.size())
         {
